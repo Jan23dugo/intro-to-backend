218283393abb6a6cca9a5646f08036758cdea939
Jan23dugo <jdugo23@gmail.com>
2025-12-03 07:41:36 +0800

    Feature: added CRUD APIs

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,9 +5,11 @@ const app = express(); // create an express app
 app.use(express.json());
 
 // Routes import
-import userRouter from './routes/user.route.js';
+import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
 
-// Route declarion 
+// Route declarion
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/posts", postRouter);
 
 export default app;
